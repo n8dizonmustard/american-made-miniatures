ff8d325f8d630c4f38153021c5d7699d7520ed73
Nathaniel Dizon <n8dizon@nathaniels-air.lan>
2021-05-26 14:41:52 -0700

    header for home page complete.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import './Header.css';
 import { Link } from 'react-router-dom';
+import HeaderHomePic from '../../pics/headerhome.png';
 
 export default function Header(){
     return (
         <>
-        <Link to='/'>Stable Acres</Link>
-        <Link to='/visit'>Visit</Link>
-        <Link to='/animals'>Animals</Link>
+        <div className='navbar'>
+            <Link to='/' className='nav-link'>Stable Acres</Link>
+            <Link to='/visit' className='nav-link'>Visit</Link>
+            <Link to='/animals' className='nav-link'>Animals</Link>
+        </div>
+        <img src={HeaderHomePic} alt='headerhome' className='header-home'/>
+        <div className='header-div'>
+            <h1 className='header-title'>Stable Acres</h1>
+            <p>Home of American Made Miniatures</p>
+        </div>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App/App';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <React.StrictMode>

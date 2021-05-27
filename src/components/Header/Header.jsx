6a6014da1d94e35ec802ca21cf4503832d2c577a
Nathaniel Dizon <n8dizon@nathaniels-air.lan>
2021-05-26 17:01:41 -0700

    adjusting styling on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
-import HeaderHomePic from '../../pics/headerhome.png';
+import HeaderHomePic from '../../pics/headers/home.png';
 
 export default function Header(){
     return (
         <>
+        <div className='parent'>
         <div className='navbar'>
             <Link to='/' className='nav-link'>Stable Acres</Link>
             <Link to='/visit' className='nav-link'>Visit</Link>
@@ -15,6 +16,7 @@ export default function Header(){
         <div className='header-div'>
             <h1 className='header-title'>Stable Acres</h1>
             <p>Home of American Made Miniatures</p>
+        </div>
         </div>
         </>
     )
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './HomePage.css';
+import { Grid, Icon, Image } from 'semantic-ui-react';
 import Header from '../../components/Header/Header';
+import AboutPic from '../../pics/homepage/about.png';
 
 export default function HomePage(){
     return(
         <>
         <Header />
-        <h1>Home Page</h1>
+        
         </>
     ) 
 }
